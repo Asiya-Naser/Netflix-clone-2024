b77f531e691dca8819a725b7cddf749b57c0fd45
Asiya-Naser <khalidmukemil@gmail.com>
2024-11-12 15:53:59 +0300

    fourth commit

--- Netflix-project/src/Component/Footer/Footer.jsx
+++ Netflix-project/src/Component/Footer/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import "../Footer/footer.css";
+import "./footer.css";
+
 const Footer = () => {
   return (
     <div className="footer_outer_container">
@@ -20,32 +21,32 @@ const Footer = () => {
               <li>Legal Notice</li>
             </ul>
           </div>
-        </div>
-        <div>
-          <ul>
-            <li>Help Center</li>
-            <li>Jobs</li>
-            <li>Cookie Prefernces</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>Gift Cards</li>
-            <li>Terms Of Use</li>
-            <li>Corporate Information</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>Media Center</li>
-            <li>Privacy</li>
-            <li>Contact Us</li>
-          </ul>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
         </div>
         <div className="service_code">
           <p>Service Code</p>
         </div>
-        <div className="copy_right">&copy;1997-2024 Netflix, Inc.</div>
+        <div className="copy-write">&copy; 1997-2024 Netflix, Inc.</div>
       </div>
     </div>
   );
--- Netflix-project/src/Component/Header/Header.jsx
+++ Netflix-project/src/Component/Header/Header.jsx
@@ -1,30 +1,35 @@
 import React from "react";
-import "../Header/header.css";
+import "./header.css";
+import NetflixLogo from "../../assets/image/NetflixLogo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
 const Header = () => {
   return (
     <div className="header_outer_container">
       <div className="header_container">
         <div className="header_left">
           <ul>
-            <li>Netflix</li>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
+
             <li>Home</li>
-            <li>TvShows</li>
+            <li>TVShows</li>
             <li>Movies</li>
             <li>Latest</li>
             <li>MyList</li>
-            <li>Browes by Languages</li>
+            <li>Browse by Languages</li>
           </ul>
         </div>
         <div className="header_right">
           <ul>
-            <li><SearchIcon/></li>
-            <li><NotificationsNoneIcon/></li>
-            <li><AccountBoxIcon/> </li>
-            <li><ArrowDropDownIcon/></li>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li> <AccountBoxIcon /> </li>
+            <li><ArrowDropDownIcon /></li>
           </ul>
         </div>
       </div>
